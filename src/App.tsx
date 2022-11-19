06cda7e5097170500846374e7e7f39a40876d041
grayzerguy <grayzerg@gmail.com>
2022-11-20 01:41:26 +0200

    first

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function App(): JSX.Element {
+  return (
+    <div className='container'>
+      <h2>Advanced Tutorial gg</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/useState/setup/oneEerrorEexample/OneErrorExample.tsx
@@ -0,0 +1,11 @@
+import "./OneErrorExample.css";
+
+function OneErrorExample(): JSX.Element {
+    return (
+        <div className="OneErrorExample">
+			
+        </div>
+    );
+}
+
+export default OneErrorExample;
